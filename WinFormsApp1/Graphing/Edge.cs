40624157d885b9f89a86b40227f5025a4ca9a0dd
Anthony Salvione <anthony.salvione@wsu.edu>
2024-04-30 13:42:43 -0700

    Created basic skeleton code for the edge class, as well as filling out their constructors for when they will be created.

--- WinFormsApp1/Graphing/Edge.cs
+++ WinFormsApp1/Graphing/Edge.cs
@@ -1,12 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="Edge.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace GraphTheorySketchPad.Graphing
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Drawing.Drawing2D;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// The public class to create an Edge.
+    /// </summary>
     public class Edge
     {
+        private Vertex vertex1; // The first vertex in the given edge.
+        private Vertex vertex2; // The second vertex in the given edge.
+        private bool current; // Checks to see if the edge is currently selected.
+        private bool isLoop; // Checks to see if the edge is in a loop or not.
+        private int edgeWidth; // Determines the length (or width) or the edge.
+        private int parallelEdge; // Determines whether or not the edge is parallel.
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Edge"/> class.
+        /// </summary>
+        /// <param name="vertex1"> First vertex. </param>
+        /// <param name="vertex2"> Second vertex. </param>
+        /// <param name="edgeWidth"> Edge width. </param>
+        public Edge(Vertex vertex1, Vertex vertex2, int edgeWidth)
+        {
+            this.vertex1 = vertex1;
+            this.vertex2 = vertex2;
+            this.edgeWidth = edgeWidth;
+        }
+
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Edge"/> class.
+        /// </summary>
+        /// <param name="vertex1"> First vertex. </param>
+        /// <param name="vertex2"> Second vertex. </param>
+        /// <param name="edgeWidth"> Edge width. </param>
+        /// <param name="parallelEdge"> The other parallel edge. </param>
+        public Edge(Vertex vertex1, Vertex vertex2, int edgeWidth, int parallelEdge)
+        {
+            this.vertex1 = vertex1;
+            this.vertex2 = vertex2;
+            this.edgeWidth = edgeWidth;
+            this.parallelEdge = parallelEdge;
+        }
     }
 }
