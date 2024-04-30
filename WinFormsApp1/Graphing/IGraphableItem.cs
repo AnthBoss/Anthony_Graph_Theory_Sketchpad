dab70b65d216d749d926839437b43df5f2356f09
Anthony Salvione <anthony.salvione@wsu.edu>
2024-04-30 14:04:49 -0700

    Adjusted naming convention and did some refactoring on account of the StyleCop warnings.

---
+++ WinFormsApp1/Graphing/IGraphableItem.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="IGraphableItem.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace GraphTheorySketchPad.Graphing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The interface for objects that will gain the interface GraphableObject.
+    /// </summary>
+    public interface IGraphableItem
+    {
+    }
+}
