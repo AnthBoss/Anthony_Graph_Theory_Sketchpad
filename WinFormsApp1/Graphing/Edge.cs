8bea5ac468c9d5fe11ffa8082a56d5eab6f81a4a
Anthony Salvione <anthony.salvione@wsu.edu>
2024-04-30 14:21:53 -0700

    Implemented the get path function for the edge class, now to add isObject functions to both the edge class and the vertex class.

--- WinFormsApp1/Graphing/Edge.cs
+++ WinFormsApp1/Graphing/Edge.cs
@@ -101,5 +101,99 @@ namespace GraphTheorySketchPad.Graphing
             return this.vertex2;
             }
         }
+
+        /// <summary>
+        /// Draws the edge on the graphics object.
+        /// </summary>
+        /// <param name="g"> graphics object to draw on. </param>
+        /// <param name="pen"> pen for drawing. </param>
+        public void Draw(Graphics g, Pen pen)
+        {
+            using (var path = this.GetGraphicsPath())
+            {
+                g.DrawPath(pen, path);
+            }
+        }
+
+        /// <summary>
+        /// Gets the current graphics path of the object.
+        /// </summary>
+        /// <returns> The graphics path of the object. </returns>
+        public GraphicsPath GetGraphicsPath()
+        {
+            GraphicsPath path = new GraphicsPath();
+            int offsetRadius = this.vertex1.GetRadius;
+
+            int x1 = this.vertex1.Coordinates.X + offsetRadius;
+            int x2 = this.vertex2.Coordinates.X + offsetRadius;
+            int y1 = this.vertex1.Coordinates.Y + offsetRadius;
+            int y2 = this.vertex2.Coordinates.Y + offsetRadius;
+
+            int parallelOffset = this.CalculateParallelOffset();
+
+            // if the coordinants are horizantal, then:
+            if (Math.Abs(x1) - Math.Abs(x2) > Math.Abs(y1) - Math.Abs(y2))
+            {
+                return this.BuildGraphicsPath(path, x1, y1, x2, y2, parallelOffset, 0);
+            }
+
+            // If the coordinants are vertical, then:
+            else
+            {
+                return this.BuildGraphicsPath(path, x1, y1, x2, y2, 0, parallelOffset);
+            }
+        }
+
+        /// <summary>
+        /// Determines the offset amount for paralell edges.
+        /// </summary>
+        /// <returns> the offset. </returns>
+        private int CalculateParallelOffset()
+        {
+            if (this.parallelEdge == 0)
+            {
+                return 0;
+            }
+            else if (this.parallelEdge % 2 == 0)
+            {
+                return -12 * this.parallelEdge;
+            }
+            else
+            {
+                return 12 * this.parallelEdge;
+            }
+        }
+
+        /// <summary>
+        /// Builds the path of the edge.
+        /// </summary>
+        /// <param name="path">base path to build on. </param>
+        /// <param name="x1"> x1 coordinate. </param>
+        /// <param name="y1"> x2 coordinate. </param>
+        /// <param name="x2"> y1 coordinate. </param>
+        /// <param name="y2"> y2 coordinate. </param>
+        /// <param name="xOffset"> the amount x is offset by. </param>
+        /// <param name="yOffset"> the amount y is offset by. </param>
+        /// <returns> the path to the edge. </returns>
+        private GraphicsPath BuildGraphicsPath(GraphicsPath path, int x1, int y1, int x2, int y2, int xOffset, int yOffset)
+        {
+            // loop
+            if (this.vertex1 == this.vertex2)
+            {
+                int dim = 35 + (this.parallelEdge * 10);
+                path.AddArc(new Rectangle(x1 - 16, y1, 30, dim), 0, 360);
+                return path;
+            }
+            else
+            {
+                PointF[] points = new PointF[3];
+                points[0] = new PointF(x1, y1);
+                points[1] = new PointF(((x1 + x2) / 2) + xOffset, ((y1 + y2) / 2) + yOffset);
+                points[2] = new PointF(x2, y2);
+
+                path.AddCurve(points);
+                return path;
+            }
+        }
     }
 }
