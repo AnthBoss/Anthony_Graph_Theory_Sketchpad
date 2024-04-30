c1e93e3d104df37cd18045aec93aaf8b32091066
Anthony Salvione <anthony.salvione@wsu.edu>
2024-04-30 13:31:24 -0700

    Addressed the current StyleCop issues.

--- WinFormsApp1/Graphing/Graph.cs
+++ WinFormsApp1/Graphing/Graph.cs
@@ -1,25 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="Graph.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace GraphTheorySketchPad.Graphing
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The main public class of Graph.
+    /// </summary>
     public class Graph
     {
-        private List<Edge> edges; //The list of all of the graph's edges.
+        private List<Edge> edges; // The list of all of the graph's edges.
         private Vertex currentVertex; // The current vertex that the user selected.
         private List<Vertex> vertices; // The list of all graph's verticies.
         private List<Edge> currentEdges; // The current list of edges that the user selected.
         private bool hasCurrentVertex; // A bool check to see whether a Vertex is selected.
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Graph"/> class.
+        /// </summary>
         public Graph()
         {
-            edges = new List<Edge>();
-            currentEdges = new List<Edge>();
-            vertices = new List<Vertex>();
-            hasCurrentVertex = false;
+            this.edges = new List<Edge>();
+            this.currentEdges = new List<Edge>();
+            this.vertices = new List<Vertex>();
+            this.hasCurrentVertex = false;
         }
     }
 }
