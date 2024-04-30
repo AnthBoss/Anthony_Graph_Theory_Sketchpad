2d6c4a22e3de737c8ec6adf93173a2e93329657d
Anthony Salvione <anthony.salvione@wsu.edu>
2024-04-30 13:54:54 -0700

    Filled out the basic skeleton code for Vertex.cs

--- WinFormsApp1/Graphing/Vertex.cs
+++ WinFormsApp1/Graphing/Vertex.cs
@@ -1,14 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Drawing.Drawing2D;
+﻿// <copyright file="Vertex.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace GraphTheorySketchPad.Graphing
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing.Drawing2D;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// The public class that initializes a vertex.
+    /// </summary>
     public class Vertex
     {
+        /// <summary>
+        /// This is to identify the given vertex.
+        /// </summary>
+        private string id;
+
+        /// <summary>
+        /// Bool to check if this is the currently selected vertex.
+        /// </summary>
+        private bool current;
+
+        /// <summary>
+        /// A coordinant value, containing where the vertex is located.
+        /// </summary>
+        private Point point;
+
+        /// <summary>
+        /// Size of the Vertex.
+        /// </summary>
+        private int radius;
+
+        private int parallelEdges;
+        private List<Edge> connectedEdges;
+        private List<Vertex> neighbors;
+
+        public Vertex(string id, Point point, int radius)
+        {
+            this.id = id;
+            this.current = false;
+            this.radius = radius;
+            this.parallelEdges = 0;
+
+            // This is for the point adjusting to the screen.
+            // This solution was found on a github forum.
+            Point p = point;
+            p.Offset(-radius, -radius);
+            this.point = p;
+
+            this.connectedEdges = new List<Edge>();
+            this.neighbors = new List<Vertex>();
+        }
     }
 }
