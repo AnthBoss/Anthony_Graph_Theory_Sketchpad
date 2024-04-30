d122e7d8a5e41e3d33cd2cdc0789366ffce956a7
Anthony Salvione <anthony.salvione@wsu.edu>
2024-04-30 13:29:53 -0700

    Created some initial skeleton code for the graph class, aswell as adding StyleCop to the project for cleaner code implementation.

--- WinFormsApp1/Graphing/Graph.cs
+++ WinFormsApp1/Graphing/Graph.cs
@@ -8,5 +8,18 @@ namespace GraphTheorySketchPad.Graphing
 {
     public class Graph
     {
+        private List<Edge> edges; //The list of all of the graph's edges.
+        private Vertex currentVertex; // The current vertex that the user selected.
+        private List<Vertex> vertices; // The list of all graph's verticies.
+        private List<Edge> currentEdges; // The current list of edges that the user selected.
+        private bool hasCurrentVertex; // A bool check to see whether a Vertex is selected.
+
+        public Graph()
+        {
+            edges = new List<Edge>();
+            currentEdges = new List<Edge>();
+            vertices = new List<Vertex>();
+            hasCurrentVertex = false;
+        }
     }
 }
